ab0b4e2a5a4fbaa89481354974c38ac4ebe0ced7
asiimwedismas <asiimwedismas@gmail.com>
2020-07-07 22:35:06 +0300

    Added genere attribute

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 # class movies is a data model
 class Movie
-  attr_reader :title, :genere, :year, :rating
+  attr_reader :title, :year, :rating, :link
 
-  def initialize(title, genere, year, rating)
+  def initialize(title, year, rating, link)
     @title = title
-    @genere = genere
     @year = year
     @rating = rating
+    @link = link
   end
 end
