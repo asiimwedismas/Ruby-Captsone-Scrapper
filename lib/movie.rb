55e679806b76ce5e2458c149c99c158b64b7f522
asiimwedismas <asiimwedismas@gmail.com>
2020-07-07 19:15:16 +0300

    Created Moive class

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
-# class movies is a data model
-# title
-# genere
-# year
-# rating
 
-# getters and setters for each varibale
+# class movies is a data model
+class Movie
+  attr_reader :title, :genere, :year, :rating
+
+  def initialize(title, genere, year, rating)
+    @title = title
+    @genere = genere
+    @year = year
+    @rating = rating
+  end
+end
