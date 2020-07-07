03fd0fc1002aa1cfcb9e599461ea44e41400f75d
asiimwedismas <asiimwedismas@gmail.com>
2020-07-07 22:34:40 +0300

    Added logic to scrap the movies

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,28 @@
-# frozen_string_literal: true
-# var_movies[]
+require './lib/scrapper'
+require './lib/movie'
 
-# ask user to select year
-# ask user to secret genera
+# You can refine your search by tweaking the variables below using the adjecent commented arrays as guideline
+# happy pirating
 
-# create new scraper with user selections
-# call scrapper to scrap
+search_term = '0' # [enter search term or 0 to leave blank]
+quailty = 'all' # [720p, 1080p, 1080p, 2160p, 3D, all for all qualities]
+genre = 'animation' # [action, adventure, animation, comedy, drama, horror, all for all genres]
+rating = '0' # [1,2,3,4,5,6,7,8,9, 0 for all ratings]
+order = 'rating' # [latest, oldest,featured, seeds, peers, year, rating, likes, alphabetical, downloads]
+year = '0' # [enter year or 0 for any year]
+lang = 'en' # [en -> english, all -> all, fr -> french]
 
-# loop throught the scrapper results
-# create new movie off of every result and store it in the movie variable
+url_link = "https://yts.mx/browse-movies/#{search_term}/#{quailty}/#{genre}/#{rating}/#{order}/#{year}/#{lang}"
+
+scraper = Scrapper.new(url_link)
+
+puts("Dislpaying #{scraper.movies.size}   Movies")
+puts
+
+scraper.movies.each do |entry|
+  puts("Title:  #{entry.title}")
+  puts("Rating: #{entry.rating}")
+  puts("Year:   #{entry.year}")
+  puts("Link:   #{entry.link}")
+  puts
+end
