cac2bf442b75565b93d3aba5628acc3c87bd8679
asiimwedismas <asiimwedismas@gmail.com>
2020-07-24 18:26:00 +0300

    testing connection to github

---
+++ lib/utility.rb
@@ -0,0 +1,17 @@
+class Utility
+  def self.validate_quailty; end
+
+  def self.validate_genre; end
+
+  def self.vaidate_rating; end
+
+  def self.vaidate_order; end
+
+  def self.vaidate_year; end
+
+  def self.vaidate_lang; end
+
+  def self.display_options
+    puts
+  end
+end
